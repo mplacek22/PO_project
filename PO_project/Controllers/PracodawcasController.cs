af375eacb0cd98c153271611a9e91ef45ec92f68
TytanMikJas <mikjas@gmail.com>
2024-01-05 20:24:35 +0100

    Wygenerowanie Kontrolerow

---
+++ PO_project/Controllers/PracodawcasController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PO_project.Data;
+using PO_project.Models;
+
+namespace PO_project.Controllers
+{
+    public class PracodawcasController : Controller
+    {
+        private readonly PwrDbContext _context;
+
+        public PracodawcasController(PwrDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pracodawcas
+        public async Task<IActionResult> Index()
+        {
+            var pwrDbContext = _context.Pracodawcy.Include(p => p.adres);
+            return View(await pwrDbContext.ToListAsync());
+        }
+
+        // GET: Pracodawcas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Pracodawcy == null)
+            {
+                return NotFound();
+            }
+
+            var pracodawca = await _context.Pracodawcy
+                .Include(p => p.adres)
+                .FirstOrDefaultAsync(m => m.PracodawcaId == id);
+            if (pracodawca == null)
+            {
+                return NotFound();
+            }
+
+            return View(pracodawca);
+        }
+
+        // GET: Pracodawcas/Create
+        public IActionResult Create()
+        {
+            ViewData["AdresId"] = new SelectList(_context.Adresy, "AdresId", "AdresId");
+            return View();
+        }
+
+        // POST: Pracodawcas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PracodawcaId,CompanyName,Description,Link,AdresId")] Pracodawca pracodawca)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pracodawca);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AdresId"] = new SelectList(_context.Adresy, "AdresId", "AdresId", pracodawca.AdresId);
+            return View(pracodawca);
+        }
+
+        // GET: Pracodawcas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Pracodawcy == null)
+            {
+                return NotFound();
+            }
+
+            var pracodawca = await _context.Pracodawcy.FindAsync(id);
+            if (pracodawca == null)
+            {
+                return NotFound();
+            }
+            ViewData["AdresId"] = new SelectList(_context.Adresy, "AdresId", "AdresId", pracodawca.AdresId);
+            return View(pracodawca);
+        }
+
+        // POST: Pracodawcas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PracodawcaId,CompanyName,Description,Link,AdresId")] Pracodawca pracodawca)
+        {
+            if (id != pracodawca.PracodawcaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pracodawca);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PracodawcaExists(pracodawca.PracodawcaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AdresId"] = new SelectList(_context.Adresy, "AdresId", "AdresId", pracodawca.AdresId);
+            return View(pracodawca);
+        }
+
+        // GET: Pracodawcas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Pracodawcy == null)
+            {
+                return NotFound();
+            }
+
+            var pracodawca = await _context.Pracodawcy
+                .Include(p => p.adres)
+                .FirstOrDefaultAsync(m => m.PracodawcaId == id);
+            if (pracodawca == null)
+            {
+                return NotFound();
+            }
+
+            return View(pracodawca);
+        }
+
+        // POST: Pracodawcas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Pracodawcy == null)
+            {
+                return Problem("Entity set 'PwrDbContext.Pracodawcy'  is null.");
+            }
+            var pracodawca = await _context.Pracodawcy.FindAsync(id);
+            if (pracodawca != null)
+            {
+                _context.Pracodawcy.Remove(pracodawca);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PracodawcaExists(int id)
+        {
+          return (_context.Pracodawcy?.Any(e => e.PracodawcaId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ PO_project/Controllers/WydzialsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PO_project.Data;
+using PO_project.Models;
+
+namespace PO_project.Controllers
+{
+    public class WydzialsController : Controller
+    {
+        private readonly PwrDbContext _context;
+
+        public WydzialsController(PwrDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Wydzials
+        public async Task<IActionResult> Index()
+        {
+            var pwrDbContext = _context.Wydzialy.Include(w => w.Lokalizacja);
+            return View(await pwrDbContext.ToListAsync());
+        }
+
+        // GET: Wydzials/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Wydzialy == null)
+            {
+                return NotFound();
+            }
+
+            var wydzial = await _context.Wydzialy
+                .Include(w => w.Lokalizacja)
+                .FirstOrDefaultAsync(m => m.WydzialId == id);
+            if (wydzial == null)
+            {
+                return NotFound();
+            }
+
+            return View(wydzial);
+        }
+
+        // GET: Wydzials/Create
+        public IActionResult Create()
+        {
+            ViewData["LokalizacjaId"] = new SelectList(_context.Lokalizacje, "LokalizacjaId", "LokalizacjaId");
+            return View();
+        }
+
+        // POST: Wydzials/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("WydzialId,Name,Abbreviation,LokalizacjaId")] Wydzial wydzial)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(wydzial);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LokalizacjaId"] = new SelectList(_context.Lokalizacje, "LokalizacjaId", "LokalizacjaId", wydzial.LokalizacjaId);
+            return View(wydzial);
+        }
+
+        // GET: Wydzials/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Wydzialy == null)
+            {
+                return NotFound();
+            }
+
+            var wydzial = await _context.Wydzialy.FindAsync(id);
+            if (wydzial == null)
+            {
+                return NotFound();
+            }
+            ViewData["LokalizacjaId"] = new SelectList(_context.Lokalizacje, "LokalizacjaId", "LokalizacjaId", wydzial.LokalizacjaId);
+            return View(wydzial);
+        }
+
+        // POST: Wydzials/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("WydzialId,Name,Abbreviation,LokalizacjaId")] Wydzial wydzial)
+        {
+            if (id != wydzial.WydzialId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(wydzial);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WydzialExists(wydzial.WydzialId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LokalizacjaId"] = new SelectList(_context.Lokalizacje, "LokalizacjaId", "LokalizacjaId", wydzial.LokalizacjaId);
+            return View(wydzial);
+        }
+
+        // GET: Wydzials/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Wydzialy == null)
+            {
+                return NotFound();
+            }
+
+            var wydzial = await _context.Wydzialy
+                .Include(w => w.Lokalizacja)
+                .FirstOrDefaultAsync(m => m.WydzialId == id);
+            if (wydzial == null)
+            {
+                return NotFound();
+            }
+
+            return View(wydzial);
+        }
+
+        // POST: Wydzials/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Wydzialy == null)
+            {
+                return Problem("Entity set 'PwrDbContext.Wydzialy'  is null.");
+            }
+            var wydzial = await _context.Wydzialy.FindAsync(id);
+            if (wydzial != null)
+            {
+                _context.Wydzialy.Remove(wydzial);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WydzialExists(int id)
+        {
+          return (_context.Wydzialy?.Any(e => e.WydzialId == id)).GetValueOrDefault();
+        }
+    }
+}

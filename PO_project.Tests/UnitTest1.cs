1ff69b1e883c6f73c4288a8e5e7436d68f474d24
TytanMikJas <mikjas@gmail.com>
2024-01-07 00:36:33 +0100

    tests

--- PO_project.Tests/UnitTest1.cs
+++ PO_project.Tests/UnitTest1.cs
@@ -1,3 +1,10 @@
+using PO_project.Controllers;
+using PO_project.Data;
+using PO_project.Enums;
+using PO_project.Migrations;
+using PO_project.Models;
+using PO_project.RecrutationCalc;
+
 namespace PO_project.Tests
 {
     public class UnitTest1
@@ -5,7 +12,7 @@ namespace PO_project.Tests
         [Fact]
         public void Test1()
         {
-
+            Assert.Equal(2, 2);
         }
     }
 }

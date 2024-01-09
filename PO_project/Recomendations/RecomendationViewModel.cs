9c47547741c1a99ed55efd0b96706f63ce761497
Maja <maja.placek451@gmail.com>
2024-01-09 11:28:01 +0100

    impl recomendations

---
+++ PO_project/Recomendations/RecomendationViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace PO_project.Recomendations
+{
+	public class RecomendationViewModel
+	{
+		public int CourseId { get; set; }
+
+        public string Course { get; set; } = string.Empty;
+
+		public string Description { get; set; } = string.Empty;
+
+		public string ChancesOfGettingIn { get; set; } = string.Empty;
+
+		public double? AvgPointThreshold { get; set; }
+
+		public double CancdidateRecruitmentIndicator { get; set; }
+
+        public double ProbabilityOfAdmission { get; set; }
+	}
+}
